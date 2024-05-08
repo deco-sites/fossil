ca3ff0607853e0137a60216a6d600a8e59867761
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-05-08 15:47:23 -0300

    feat:task/productShelf - add buttons slider top

--- components/product/ProductShelf.tsx
+++ components/product/ProductShelf.tsx
@@ -56,103 +56,128 @@ function ProductShelf({
     5: "w-1/5",
   };
   return (
-    <div class="w-full relative max-w-screen-2xl  m-auto py-8 flex flex-col gap-6 lg:py-10 px-3 lg:px-4">
-      <Header
-        title={title || ""}
-        description={description || ""}
-        fontSize={layout?.headerfontSize || "Large"}
-        alignment={layout?.headerAlignment || "center"}
-      />
+    <div class="w-full max-w-screen-2xl  m-auto py-8 flex flex-col gap-6 lg:py-10 px-3 lg:px-4">
+      <div class="w-full relative">
+        <Header
+          title={title || ""}
+          description={description || ""}
+          fontSize={layout?.headerfontSize || "Large"}
+          alignment={layout?.headerAlignment || "center"}
+        />
 
-      <div
-        id={id}
-        class={clx(
-          "grid",
-          (layout?.showArrows && device === "desktop") &&
-            "grid-cols-[48px_1fr_48px] sm:grid-cols-[70px_1fr_70px] grid-rows-[1fr_48px_1fr_64px]",
-          "px-0 w-full",
-        )}
-      >
-        <Slider class="carousel carousel-center sm:carousel-end row-[1/-2] ">
-          {products?.map((product, index) => (
-            <Slider.Item
-              index={index}
-              class={clx(
-                "carousel-item",
-                slideDesktop[layout?.numberOfSliders?.desktop ?? 3],
-                slideMobile[layout?.numberOfSliders?.mobile ?? 1],
-              )}
-            >
-              <ProductCard
-                product={product}
-                itemListName={title}
-                platform={platform}
+        <div
+          id={id}
+          class={clx(
+            "grid ",
+            (layout?.showArrows && device === "desktop") &&
+              "grid-cols-[48px_1fr_48px] sm:grid-cols-[70px_1fr_70px] grid-rows-[1fr_48px_1fr_64px]",
+            "px-0 w-full",
+          )}
+        >
+          <Slider class="carousel carousel-center sm:carousel-end row-[1/-2] ">
+            {products?.map((product, index) => (
+              <Slider.Item
                 index={index}
-                device={device}
-              />
-            </Slider.Item>
-          ))}
-        </Slider>
+                class={clx(
+                  "carousel-item",
+                  slideDesktop[layout?.numberOfSliders?.desktop ?? 3],
+                  slideMobile[layout?.numberOfSliders?.mobile ?? 1],
+                )}
+              >
+                <ProductCard
+                  product={product}
+                  itemListName={title}
+                  platform={platform}
+                  index={index}
+                  device={device}
+                />
+              </Slider.Item>
+            ))}
+          </Slider>
 
-        {/** Dots */}
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `
+          {/** Dots */}
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
           @property --dot-progress {
             syntax: '<percentage>';
             inherits: false;
             initial-value: 0%;
           }
           `,
-          }}
-        />
-        <ul class="carousel justify-center col-span-full gap-4 z-10 row-start-4 col-span-[1/2]">
-          {products?.map((_, index) => (
-            <li
-              class={`carousel-item   md:${
-                ((index === 0) || (index % 4 === 0)) ? "" : "hidden"
-              } ${((index === 0) || (index % 2 === 0)) ? "" : "hidden"}`}
-            >
-              <Slider.Dot index={index}>
-                <div class="py-5">
-                  <div class=" w-[10px] h-[10px] md:w-[14px] md:h-[14px] sm:w-[16px] sm:h-[16px] group-disabled:bg-primary rounded-full bg-transparent border border-[#a4a4a4]" />
-                </div>
-              </Slider.Dot>
-            </li>
-          ))}
-        </ul>
+            }}
+          />
+          <ul class="carousel justify-center col-span-full gap-4 z-10 row-start-4 col-span-[1/2]">
+            {products?.map((_, index) => (
+              <li
+                class={`carousel-item   md:${
+                  ((index === 0) || (index % 4 === 0)) ? "" : "hidden"
+                } ${((index === 0) || (index % 2 === 0)) ? "" : "hidden"}`}
+              >
+                <Slider.Dot index={index}>
+                  <div class="py-5">
+                    <div class=" w-[10px] h-[10px] md:w-[14px] md:h-[14px] sm:w-[16px] sm:h-[16px] group-disabled:bg-primary rounded-full bg-transparent border border-[#a4a4a4]" />
+                  </div>
+                </Slider.Dot>
+              </li>
+            ))}
+          </ul>
 
-        {(layout?.showArrows && device === "desktop") && (
-          <>
-            <div class="hidden md:flex items-center justify-center z-10 col-start-1 row-start-2">
-              <Slider.PrevButton class="absolute w-12 h-12 flex justify-center items-center">
-                <Icon size={24} id="ChevronLeft" strokeWidth={3} class="w-5" />
+          {(layout?.showArrows && device === "desktop") && (
+            <>
+              <div class="hidden md:flex items-center justify-center z-10 col-start-1 row-start-2">
+                <Slider.PrevButton class="absolute w-12 h-12 flex justify-center items-center">
+                  <Icon
+                    size={25}
+                    id="ChevronLeft"
+                    strokeWidth={3}
+                    class="w-5"
+                  />
+                </Slider.PrevButton>
+              </div>
+              <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
+                <Slider.NextButton class="absolute w-12 h-12 flex justify-center items-center">
+                  <Icon size={25} id="ChevronRight" strokeWidth={3} />
+                </Slider.NextButton>
+              </div>
+            </>
+          )}
+
+          {/**buttons top */}
+          <div class="absolute top-0 right-0  w-48 h-2 flex items-center justify-between">
+            <div class="hidden w-1/2 md:flex items-center  justify-between z-10 col-start-1 row-start-2 border-r-2 border-solid border-primary">
+              <Slider.PrevButton class=" w-full  flex justify-center items-center">
+                <Icon size={25} id="ChevronLeft" strokeWidth={3} class="w-5" />
+                <span class="text-base text-primary font-medium">Anterior</span>
               </Slider.PrevButton>
             </div>
-            <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
-              <Slider.NextButton class="absolute w-12 h-12 flex justify-center items-center">
-                <Icon size={24} id="ChevronRight" strokeWidth={3} />
+
+            <div class="flex items-center w-1/2  justify-center z-10 col-start-3 row-start-2">
+              <Slider.NextButton class="w-full flex justify-center items-center">
+                <span class="text-base text-primary font-medium">Próximo</span>{" "}
+                <Icon size={25} id="ChevronRight" strokeWidth={3} />
               </Slider.NextButton>
             </div>
-          </>
-        )}
-        <SliderJS rootId={id} />
-        <SendEventOnView
-          id={id}
-          event={{
-            name: "view_item_list",
-            params: {
-              item_list_name: title,
-              items: products.map((product, index) =>
-                mapProductToAnalyticsItem({
-                  index,
-                  product,
-                  ...(useOffer(product.offers)),
-                })
-              ),
-            },
-          }}
-        />
+          </div>
+
+          <SliderJS rootId={id} />
+          <SendEventOnView
+            id={id}
+            event={{
+              name: "view_item_list",
+              params: {
+                item_list_name: title,
+                items: products.map((product, index) =>
+                  mapProductToAnalyticsItem({
+                    index,
+                    product,
+                    ...(useOffer(product.offers)),
+                  })
+                ),
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
