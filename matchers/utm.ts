2af5ddec76350d0563123c9970da73efa6e93c6b
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-05-21 14:02:37 -0300

    feat: task/search - add matchers Utm

---
+++ matchers/utm.ts
@@ -0,0 +1,18 @@
+import { MatchContext } from 'deco/blocks/matcher.ts';
+
+interface UtmProps {
+    /**
+      @description The name of the campaign
+     **/
+    campaignName?: string;
+}
+
+/**
+ * Checks if the campaign name matches the utm_campaign parameter in the URL
+ */
+export default function Utm({ campaignName }: UtmProps, ctx: MatchContext): boolean {
+    const url = new URL(ctx.request.url);
+    const utmCampaign = url.searchParams.get('utmi_pc');
+     console.log((utmCampaign !== null && utmCampaign === campaignName) || utmCampaign === 'BuscaFullText')
+    return (utmCampaign !== null && utmCampaign === campaignName) || utmCampaign === 'BuscaFullText';
+}
