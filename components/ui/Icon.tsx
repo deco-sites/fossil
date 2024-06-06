a74f7f51d65176719e7a51271027d3fa3d23eb6a
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-06-06 16:42:38 -0300

    fix : Header component with new Alert feature, added isShow prop, and modified layout and styles; added new Icon 'XAlert' and updated sprites.svg; refactored Header.tsx and Alert.tsx files."

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -55,7 +55,8 @@ export type AvailableIcons =
   | "YouTube"
   | "Pinterest"
   | "Phone"
-  | "Email";
+  | "Email"
+  | "XAlert"; 
 
 interface Props extends JSX.SVGAttributes<SVGSVGElement> {
   /**
