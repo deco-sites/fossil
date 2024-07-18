963bd43d9d68d1b6db4c41e88a5157f768b91dcc
decobot <thiago.pereira@agencian1.com.br>
2024-07-18 18:58:15 +0000

    Release staging environment with 0 changes made by

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -77,9 +77,10 @@ import * as $$$$$$64 from "./sections/Product/SearchResult.tsx";
 import * as $$$$$$65 from "./sections/Product/SearchResultView.tsx";
 import * as $$$$$$66 from "./sections/Product/ShelfWithImage.tsx";
 import * as $$$$$$67 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$68 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$69 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$70 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$68 from "./sections/Seo/SeoPLPV6.tsx";
+import * as $$$$$$69 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$70 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$71 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -157,9 +158,10 @@ const manifest = {
     "site/sections/Product/SearchResultView.tsx": $$$$$$65,
     "site/sections/Product/ShelfWithImage.tsx": $$$$$$66,
     "site/sections/Product/Wishlist.tsx": $$$$$$67,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$68,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$69,
-    "site/sections/Theme/Theme.tsx": $$$$$$70,
+    "site/sections/Seo/SeoPLPV6.tsx": $$$$$$68,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$69,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$70,
+    "site/sections/Theme/Theme.tsx": $$$$$$71,
   },
   "matchers": {
     "site/matchers/utm.ts": $$$$$$$0,
