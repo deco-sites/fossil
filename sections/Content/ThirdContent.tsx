8b58cf18f599b972742b0a7dda4de6c5339f2777
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-11-22 16:51:15 -0400

    11366 - change ThirdContent props optional

--- sections/Content/ThirdContent.tsx
+++ sections/Content/ThirdContent.tsx
@@ -5,35 +5,35 @@ export interface Props {
   /**
    * @title Título do componente
    */
-  title: string;
+  title?: string;
   /**
    * @title Link de script para head
    * @description <script type="text/javascript" src=""></script> Colocar link do `src`
    */
-  script_head: string;
+  script_head?: string;
   /**
    * @title Link de CSS para head
    * @description <link type="text/css" rel="stylesheet" href="" /> Colocar link do `href`
    */
-  css_head: string;
+  css_head?: string;
   /**
    * @title Código Javascript para BODY
    * @description Colocar a tag com o conteúdo dentro <script></script>
    * @format html
    */
-  script: string;
+  script?: string;
   /**
    * @title Código CSS para BODY
    * @description Colocar a tag com o conteúdo dentro <style></l>
    * @format html
    */
-  css: string;
+  css?: string;
   /**
    * @title Código HTML para BODY
    * @description Colocar a tag com o conteúdo dentro <body></body>
    * @format html
    */
-  html: string;
+  html?: string;
 }
 
 export default function ThirdContent(props: Props) {
