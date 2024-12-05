5d39bed8273c5befbb9fa728163445d2934f5f63
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-12-05 11:41:54 -0400

    11366 - change vtex AddToCartButton remove marketing_data

--- components/product/AddToCartButton/vtex.tsx
+++ components/product/AddToCartButton/vtex.tsx
@@ -1,32 +1,21 @@
-import { useCart } from 'apps/vtex/hooks/useCart.ts';
-import Button, { Props as BtnProps } from './common.tsx';
-import {
-  getMarketingDataByClientCookies,
-  sendMarketingData,
-} from '../../../sdk/useMarketingData.ts';
+import { useCart } from "apps/vtex/hooks/useCart.ts";
+import Button, { Props as BtnProps } from "./common.tsx";
 
-export interface Props extends Omit<BtnProps, 'onAddItem'> {
+export interface Props extends Omit<BtnProps, "onAddItem"> {
   seller: string;
   productID: string;
 }
 
 function AddToCartButton({ seller, productID, eventParams }: Props) {
-  const { addItems, cart } = useCart();
-  const coupon = cart.value?.marketingData?.coupon;
-  const onAddItem = () => {
-    const marketing_data = getMarketingDataByClientCookies(coupon);
-    sendMarketingData(marketing_data);
-
-    return addItems({
-      orderItems: [
-        {
-          id: productID,
-          seller: seller,
-          quantity: 1,
-        },
-      ],
+  const { addItems } = useCart();
+  const onAddItem = () =>
+    addItems({
+      orderItems: [{
+        id: productID,
+        seller: seller,
+        quantity: 1,
+      }],
     });
-  };
 
   return <Button onAddItem={onAddItem} eventParams={eventParams} />;
 }
