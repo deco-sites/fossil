c44be8efe2a3603b3f21d68062c868e1fb4f77a0
guitavano <tavano62@gmail.com>
2025-08-29 14:52:15 -0300

    nick-jonas

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "$fresh/server.ts";
 import { plugins } from "deco/plugins/deco.ts";
 import manifest from "./manifest.gen.ts";
+import { mcpServer } from "@deco/mcp";
 // import tailwind from "./tailwind.config.ts";
 
 export default defineConfig({
   plugins: plugins({
     manifest,
+    useServer: (deco, hono) => {
+      hono.use(
+        "/*",
+        mcpServer(deco, {
+          include: [
+            "vtex/loaders/categories/tree.ts",
+          ],
+        }),
+      );
+    },
   }),
 });
