58a8e200c3be3235db92ea57258e75e100a92700
Fernando Nobuyasu <fernando.nobuyasu@n1.ag>
2025-12-26 16:34:57 -0300

    13138 - create CategoryCarousel island

---
+++ islands/CategoryCarousel.tsx
@@ -0,0 +1,8 @@
+import type { CategoryCarouselProps } from "../sections/Category/CategoryCarousel.tsx";
+import Component from "../sections/Category/CategoryCarousel.tsx";
+
+function Island(props: CategoryCarouselProps) {
+  return <Component {...props} />;
+}
+
+export default Island;
