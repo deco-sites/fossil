95fbe7a7790d8092540f9d9af86e0e7350801546
Fernando Nobuyasu <fernando.nobuyasu@n1.ag>
2025-12-26 16:33:14 -0300

    13138 - delete CategoryTabs island

--- islands/CategoryTabs.tsx
+++
@@ -1,8 +0,0 @@
-import type { Props } from "../sections/Category/CategoryTabs.tsx";
-import Component from "../sections/Category/CategoryTabs.tsx";
-
-function Island(props: Props) {
-  return <Component {...props} device={props.device || "desktop"} />;
-}
-
-export default Island;
